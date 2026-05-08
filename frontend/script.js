025cad3b8da4bea5beeb7ba54e11410468ee948d
nolanbiggers <nolanbiggers@student.olympic.edu>
2026-05-07 20:10:53 -0700

    Basic Frontend

---
+++ frontend/script.js
@@ -0,0 +1,21 @@
+const button = document.getElementById("submitCode");
+
+button.addEventListener("click", sendToBackend);
+
+function sendToBackend() {
+    let text = document.getElementById("textBox").value;
+
+    fetch("http://127.0.0.1:5000/submit", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ text: text })
+    })
+    .then(response => response.json())
+    .then(data => {
+        const output = document.getElementById("output"); 
+        output.classList.remove("hidden");
+        output.innerText = data.message;
+    });
+}
