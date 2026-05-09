cfb75955face81b96f43a777e7fb6df2df1a5af6
amandabradburn-ciss <mandibradburn15@gmail.com>
2026-05-09 08:46:41 -0700

    Fixed fetch comment

--- frontend/script.js
+++ frontend/script.js
@@ -7,7 +7,7 @@ function sendToBackend() {
 
     //for live website, replace other fetch
     //fetch("https://web.williamodavis.me/submit", {
-    fetch("https://web.williamodavis.me/submit", {
+    fetch("http://127.0.0.1:5000/submit", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
