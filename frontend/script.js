c68242bc9eec4280ab2491de5fa85869c816dd85
nolanb24wwu <biggern2@wwu.edu>
2026-05-22 14:01:04 -0700

    removed choose, added beta, and added CWE link

--- frontend/script.js
+++ frontend/script.js
@@ -8,16 +8,17 @@ function sendToBackend() {
     const language = document.getElementById("languageSelect").value;
     const passcode = document.getElementById("passcodeBox").value;
 
+    // Basic input validation
     if (text.length === 0) {
         alert("Input cannot be empty");
         return;
-    }
+    } 
 
     if (text.length > 5000) {
         alert("Input too long");
         return;
-    }
-
+    } 
+    // diable button and change text while waiting for response
     button.disabled = true;
     button.textContent = "Sending...";
 
@@ -37,23 +38,23 @@ function sendToBackend() {
     const output = document.getElementById("output");
 
     output.textContent = "";
-     
+     // Check for status in the response and display it if it's not "ok"
     if (data.status && data.status !== "ok") {
         output.textContent += `Status: ${data.status}\n\n`;
     }
-
+    // divides up hjson string into individual vulnerabilities and formats them for display
     let vulnerabilities = data.message;
     if (typeof vulnerabilities === "string") {
     try {
         vulnerabilities = JSON.parse(vulnerabilities);
     } catch (e) {
-        vulnerabilities = [];
+        vulnerabilities = []; // If parsing fails, treat it as no vulnerabilities
     }
     }
 
     if (vulnerabilities.length === 0) {
     output.textContent = "No vulnerabilities found.";
-    return; // 🔥 STOP HERE
+    return; // Exit early if no vulnerabilities to display
     }
 
     vulnerabilities.forEach((item, index) => {
